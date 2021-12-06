9c89cbf6c8b49d10e8a6fd7d82ebdf6d7543eebd
Niloy-Sumon <niloyabidur@gmail.com>
2021-12-07 05:10:02 +0600

    contact page done

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import emailjs from 'emailjs-com';
+import ContactInfo from './ContactInfo';
+import './Contact.css'
 
 const Contact = () => {
+    const [msg, setMsg] = useState(undefined);
+    const [err, setErr] = useState(undefined);
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_rxquisq', 'template_6inygxu', form.current, 'user_1yWzUpfEaQGRwdnsOpTf3')
+            .then((result) => {
+                console.log(result);
+                setTimeout(() => setMsg(result), 1000)
+            }, (error) => {
+                console.log(error.text);
+                setErr(error)
+            });
+        e.target.reset()
+    }
     return (
         <div>
-            <h6>Hello contact</h6>
+            <div className="container">
+            {msg && <p className='text-success'>"Successfully send your Email."</p>}
+                    {err && <p>"Error"</p>}
+                <section class="mb-4">
+                    <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+                    <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
+                        a matter of hours to help you.</p>
+                    <div class="row">
+
+                        <div class="col-md-9 mb-md-0 mb-5">
+                            <div className="container">
+                                <form ref={form} onSubmit={sendEmail}>
+                                    <div className="form-group mb-3">
+                                        <label>Name</label>
+                                        <input type="text" name="name" class="form-control" placeholder="Enter Name" />
+                                    </div>
+                                    <div className="form-group mb-3">
+                                        <label>Email</label>
+                                        <input type="email" name="email" class="form-control" placeholder="Enter Email" />
+                                    </div>
+                                    <div className="form-group mb-3">
+                                        <label>Subject</label>
+                                        <input type="text" name="subject" class="form-control" placeholder="Subject" />
+                                    </div>
+                                    <div className="form-group mb-3">
+                                        <label>Message</label>
+                                        <textarea name="message" class="form-control" placeholder="Message" />
+                                    </div>
+                                    <input type="submit" value="Send" class="btn btn-primary mi-button" />
+
+                                </form>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3 ">
+                            <ContactInfo></ContactInfo>
+                        </div>
+
+                    </div>
+
+                </section>
+            </div>
         </div>
     );
 };
---
+++ src/components/Contact/ContactInfo.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './Contact.css'
+
+import {BsWhatsapp, BsFillEnvelopeFill, BsPinMap } from "react-icons/bs";
+
+const ContactInfo = () => {
+    return (
+        <div>
+            <ul class="list-unstyled mb-0">
+                <li>
+                    <div className="contact-infoblock">
+                        <div className="row">
+                            <div className="contact-info-grid col-md-3 d-flex justify-content-center align-items-center">
+                                <BsWhatsapp />
+                            </div>
+                            <div className="contact-info-grid col-md-9 m-0 p-0">
+                                <h5>Phone/Whatsapp</h5>
+                                <p>+88 01740100570</p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div className="contact-infoblock">
+                        <div className="row">
+                            <div className="contact-info-grid col-md-3 d-flex justify-content-center align-items-center">
+                                <BsFillEnvelopeFill />
+                            </div>
+                            <div className="contact-info-grid col-md-9 m-0 p-0">
+                                <h5>Email</h5>
+                                <p>niloybdesh@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+
+                <li>
+                <div className="contact-infoblock">
+                        <div className="row">
+                            <div className="contact-info-grid col-md-3 d-flex justify-content-center align-items-center">
+                                <BsPinMap />
+                            </div>
+                            <div className="contact-info-grid col-md-9 m-0 p-0">
+                                <h5>Phone</h5>
+                                <p>Sylhet, Bangladesh</p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default ContactInfo;
