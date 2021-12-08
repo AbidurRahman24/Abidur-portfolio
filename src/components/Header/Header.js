01e5d2e3de9077b072d99e4c81420b21f38008c2
Niloy-Sumon <niloyabidur@gmail.com>
2021-12-09 05:23:53 +0600

    go to develop

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.css'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <>
-            <nav class=" navbar navbar-expand-lg navbar-light bg-light">
+            <nav class=" navbar navbar-expand-lg navbar-light ">
                 <div class="container">
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -13,21 +14,16 @@ const Header = () => {
                     <div class="collapse navbar-collapse font-weight-bold" id="navbarTogglerDemo01">
                         <a class="navbar-brand" href="/">ABIDUR</a>
                         <ul class="navbar-nav ms-auto">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/about">About</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/resume">Resume</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/portfolios">Portfolios</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/contact">Contact</a>
-                            </li>
+                            <Link class="nav-item nav-link active" to="/">Home
+                            </Link >
+                            <Link  class="nav-item nav-link" to="/about">About
+                            </Link >
+                            <Link  class="nav-item nav-link" to="/resume">Resume
+                            </Link >
+                            <Link  class="nav-item nav-link" to="/portfolios">Portfolios
+                            </Link >
+                            <Link  class="nav-item nav-link" to="/contact">Contact
+                            </Link >
                         </ul>
                     </div>
                 </div>
--- src/components/Portfolio/PortfolioDetails/PortfolioDetails.js
+++ src/components/Portfolio/PortfolioDetails/PortfolioDetails.js
@@ -5,12 +5,17 @@ const PortfolioDetails = (props) => {
     const { img, title, url } = props.data
     return (
         <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
-            <div className="row p-3">
-                <div class="card" style={{ width: '18rem' }}>
+            <div className="row p-3 m-3 image-container">
+                <div class="card image" style={{ width: '22rem' }}>
                     <img class="card-img-top" src={img} alt="" />
                     <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div class="overlay">
+                            <h5 class="text">{title}</h5>
+                        </div>
                     </div>
+                </div>
+                <div className="project-url">
+                    <a href={url}>Live Site</a>
                 </div>
             </div>
         </div>
