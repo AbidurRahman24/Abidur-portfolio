3028f8f2f0acb9bd47a8ffef31901b1a1546caf9
Niloy-Sumon <niloyabidur@gmail.com>
2021-12-09 23:40:54 +0600

    deploy

--- src/components/Portfolio/PortfolioDetails/PortfolioDetails.js
+++ src/components/Portfolio/PortfolioDetails/PortfolioDetails.js
@@ -5,7 +5,7 @@ const PortfolioDetails = (props) => {
     const { img, title, url } = props.data
     return (
         <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
-            <div className="row p-3 m-3 image-container">
+            <div className="row  image-container">
                 <div class="card image" style={{ width: '22rem' }}>
                     <img class="card-img-top" src={img} alt="" />
                     <div class="card-body">
@@ -14,9 +14,9 @@ const PortfolioDetails = (props) => {
                         </div>
                     </div>
                 </div>
-                <div className="project-url">
-                    <a href={url}>Live Site</a>
-                </div>
+            </div>
+            <div className="p-3">
+            <a href={url}>Live Site</a>
             </div>
         </div>
     );
