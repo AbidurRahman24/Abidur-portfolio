997b3937666919e04d251d48e60f5da895a42d1a
Niloy-Sumon <niloyabidur@gmail.com>
2022-01-02 19:18:43 +0600

    deploy

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,8 +9,7 @@ const Home = () => {
             <div style={{ height: '600px', }} className="row d-flex align-items-center">
                 <div className='text-center'>
                     <h1 className='header-text pb-2'>Hi, I am <span className='theme-color'> Abidur Rahman </span></h1>
-                    <p>I am a frontend web developer. I can provide clean code and pixel perfect design. I also make website more & more interactive with web animations.</p>
-                    <p>I have more than 1.5 years of experience in the web development sector. I have developed Doctors Portal Website, Gym Fitness, E Commerce, Booking website.</p>
+                    <p className='portfolio-paragrap' >I am a frontend web developer. I can provide clean code and pixel perfect design. <br /> I also make website more & more interactive with web animations. I have more than 1.5 years of experience in the web development sector. I have developed Doctors Portal Website, Gym Fitness, E Commerce, Booking website.</p>
                     <a href={cv} className='mi-button'>DOWNLOAD CV</a>
                 </div>
             </div>
